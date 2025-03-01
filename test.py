ec73612e9049ccae55ecfcf7c2e909227349b7c3
AyaIsmail284 <ayaismail284@gmail.com>
2025-03-01 13:11:41 -0500

    updated test.py

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ response_main = requests.get("https://mini-proj-bot.onrender.com")
 print('Web Application Response:\n', response_main.text, '\n\n')
 
 
-data = {"text":"tell me about tufts"}
+data = {"text":"Hi there!"}
 response_llmproxy = requests.post("https://mini-proj-bot.onrender.com/query", json=data)
 print('LLMProxy Response:\n', response_llmproxy.text)
