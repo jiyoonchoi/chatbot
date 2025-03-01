560fcd2a724f43150933b5cfc63f4d61b1cdd57e
AyaIsmail284 <ayaismail284@gmail.com>
2025-03-01 13:52:26 -0500

    updated test with username

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ response_main = requests.get("https://balanced-bess-jiyoon-648a0821.koyeb.app/")
 print('Web Application Response:\n', response_main.text, '\n\n')
 
 
-data = {"text":"tell me about tufts"}
+data = {"text":"tell me about tufts", "user_name":"Aya", "bot":False}
 response_llmproxy = requests.post("https://balanced-bess-jiyoon-648a0821.koyeb.app/query", json=data)
 print('LLMProxy Response:\n', response_llmproxy.text)
