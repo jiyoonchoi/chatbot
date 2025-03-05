be2accee0911f9b78f75c026ec4507af832cd91a
AyaIsmail284 <ayaismail284@gmail.com>
2025-03-05 09:38:18 -0500

    fixed test

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import requests
 
-response_main = requests.get("https://balanced-bess-jiyoon-648a0821.koyeb.app/")
+response_main = requests.get("https://mini-proj-bot.onrender.com")
 print('Web Application Response:\n', response_main.text, '\n\n')
 
 
 data = {"text":"tell me about tufts"}
-response_llmproxy = requests.post("https://balanced-bess-jiyoon-648a0821.koyeb.app/query", json=data)
+response_llmproxy = requests.post("https://mini-proj-bot.onrender.com/query", json=data)
 print('LLMProxy Response:\n', response_llmproxy.text)
